8ed066afa9728fed37b64586e216469c0c1678b6
iahia <iahiaimon555@gmail.com>
2025-03-01 02:51:29 +0600

    Uploading

--- home/forms.py
+++ home/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from . import models
 
-# from .models import ContactModel
-# from home.models import ContactModel
-
 class ContactForm(forms.ModelForm):
     class Meta:
         model = models.ContactModel
